bd5c2429c86b25a7e0a90af446abfa5f4047e92b
Hpday <>
2026-07-06 10:33:16 +0800

    feat(s6000): add app skeleton

---
+++ s6000/apps.py
@@ -0,0 +1,7 @@
+﻿from django.apps import AppConfig
+
+
+class S6000AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 's6000'
+    verbose_name = 'S6000 接口代理'
